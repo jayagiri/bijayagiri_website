f12765e3c4d1a8b34dec4e8d5c4048e749f8fae5
jaya.jc999@gmail.com <jaya.jc999@gmail.com>
2023-02-07 16:18:49 +0545

    done

---
+++ src/components/Home.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import Navbar from './Navbar'
+import myimg from '../img/img1.jpg'
+
+const home = () => {
+  return (
+    <div > 
+
+<div >
+<img  className="bg-contain bg-center" src={myimg} alt=""/>
+      </div>
+
+    </div>
+  )
+}
+
+export default home
